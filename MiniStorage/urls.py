0daeb9293bd57c97085ba6ca1457fa5304563120
SergeyLebidko <sergeyler@gmail.com>
2020-08-27 21:06:38 +0300

    Добавлена страница для входа пользователей на сайт

--- MiniStorage/urls.py
+++ MiniStorage/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from main.views import Login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('login/', Login.as_view(), name='login')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 
 
+class Login(LoginView):
+    template_name = 'main/login.html'
+
+
+@login_required(login_url='login')
 def index(request):
     return render(request, 'main/index.html', context={})
