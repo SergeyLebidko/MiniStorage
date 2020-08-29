80a25b24bbb713c5010d4306d24d1390693c7743
SergeyLebidko <sergeyler@gmail.com>
2020-08-29 18:55:30 +0300

    Добавлен rest_framework

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import index
+from rest_framework import routers
+from .views import index, ProductViewSet
+
 
 urlpatterns = [
     path('', index, name='index')
 ]
+
+router = routers.SimpleRouter()
+router.register('api/products', ProductViewSet)
+urlpatterns.extend(router.urls)
--- main/views.py
+++ main/views.py
@@ -2,6 +2,9 @@ from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets
+from .serializers import ProductSerializer
+from .models import Product
 
 
 class Login(LoginView):
@@ -15,3 +18,10 @@ class Logout(LogoutView):
 @login_required(login_url='login')
 def index(request):
     return render(request, 'main/index.html', context={})
+
+# Методы API
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
