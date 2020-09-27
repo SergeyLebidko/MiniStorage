c9bd8c3115ace991b786e4c6b19a7d3104adad6d
SergeyLebidko <it.sergeyler@mail.ru>
2020-09-27 16:51:22 +0300

    Рефакторинг js-скриптов

--- main/static/main/common.js
+++ main/static/main/common.js
@@ -11,19 +11,3 @@ function closeModal($modal) {
     document.body.style.overflow = "auto";
     $modal.hide("normal");
 }
-
-function createErrorText(jqXHR) {
-    if ('responseText' in jqXHR) {
-        if (jqXHR.status >= 500) {
-            return `(${jqXHR.status}. ${jqXHR.statusText}) Внутренняя ошибка сервера`;
-        } else {
-            let responseObj = JSON.parse(jqXHR.responseText);
-            let errors = [];
-            for (let key of Object.keys(responseObj)) {
-                errors.push(responseObj[key]);
-            }
-            return `(${jqXHR.status}. ${jqXHR.statusText}) ${errors.join(" ")}`;
-        }
-    }
-    return "Сервер не доступен..."
-}
